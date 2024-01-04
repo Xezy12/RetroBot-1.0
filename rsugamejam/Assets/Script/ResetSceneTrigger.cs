686b6512dfc904794a0ab92ef23c99ee67c4a82d
Xezy12 <wasurath2007@gmail.com>
2024-01-05 02:27:15 +0700

    reset menu + panel animation

--- rsugamejam/Assets/Script/ResetSceneTrigger.cs
+++ rsugamejam/Assets/Script/ResetSceneTrigger.cs
@@ -1,14 +1,42 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ResetSceneTrigger : MonoBehaviour
 {
+    public GameObject popupAfterTheEnd;
+    public Button playAgainButton;
+    public Button quitButton;
+    public Animator popupAfterTheEndAnimator;
+
+    void Start()
+    {
+        popupAfterTheEnd.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other != null && other.gameObject != null && other.CompareTag("Player"))
         {
-            // Reset the scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // Trigger the "ShowPopup" animation
+            popupAfterTheEnd.SetActive(true);
+            popupAfterTheEndAnimator.SetTrigger("ShowPopup");
+
+            // Assign functions to buttons
+            playAgainButton.onClick.AddListener(PlayAgain);
+            quitButton.onClick.AddListener(QuitGame);
         }
+    }
+
+    // Function to restart the scene
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Function to quit the game
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
